1d8f8a1413f277e8435a6e74548e4e8a04f2a61e
igrekus <upload234@mail.ru>
2018-12-13 15:11:52 +0300

    Initial commit

---
+++ domain.py
@@ -0,0 +1,373 @@
+import time
+
+import serial
+from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot, QThreadPool
+
+# MOCK
+def_mock = True
+
+
+class MeasureContext:
+
+    def __init__(self, model):
+        self._model = model
+
+    def __enter__(self):
+        print('\nacquire analyzer context')
+        self._model._analyzer.init_instrument()
+
+    def __exit__(self, *args):
+        print('\nexit analyzer context')
+        self._model._analyzer.finish()
+
+
+class InstrumentManager:
+
+    def __init__(self):
+        self._analyzer_addr = 'TCPIP::192.168.0.3::INSTR'
+
+        self._programmer = None
+        self._analyzer = None
+
+        self._available_ports = list()
+
+    def _find_ports(self):
+        for port in [f'COM{i+1}' for i in range(256)]:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                self._available_ports.append(port)
+            except (OSError, serial.SerialException):
+                pass
+
+    def _find_spi_port(self):
+        for port in self._available_ports:
+            s = serial.Serial(port=port, baudrate=9600, timeout=0.5)
+            if s.is_open:
+                s.write(b'<n>')
+                ans = s.read(9)
+                s.close()
+                if b'SPI' in ans:
+                    return port
+        else:
+            return ''
+
+    def _find_parallel_port(self):
+        for port in self._available_ports:
+            s = serial.Serial(port=port, baudrate=9600, timeout=0.5)
+            if s.is_open:
+                s.write(b'#NAME')
+                ans = s.read(9)
+                s.close()
+                if b'ARDUINO' in ans:
+                    return port
+        else:
+            return ''
+
+    def _find_programmer(self):
+        if def_mock:
+            from arduino.arduinospimock import ArduinoSpiMock
+            self._programmer = ArduinoSpiMock()
+            return
+
+        port = self._find_parallel_port()
+        if port:
+            from arduino.arduinoparallel import ArduinoParallel
+            self._programmer = ArduinoParallel(port=port, baudrate=9600, parity=serial.PARITY_NONE, bytesize=8,
+                                               stopbits=serial.STOPBITS_ONE, timeout=0.5)
+            return
+
+        port = self._find_spi_port()
+        if port:
+            from arduino.arduinospi import ArduinoSpi
+            self._programmer = ArduinoSpi(port=port, baudrate=115200, parity=serial.PARITY_NONE, bytesize=8,
+                                          stopbits=serial.STOPBITS_ONE, timeout=1)
+
+    def _find_analyzer(self):
+        if def_mock:
+            from instr.obzor304mock import Obzor304Mock
+            self._analyzer = Obzor304Mock(self.analyzer_addr)
+            return
+
+        from instr.obzor304 import Obzor304
+        try:
+            self._analyzer = Obzor304(self._analyzer_addr)
+        except Exception as ex:
+            print(f'analyzer error: {ex}')
+
+    def find(self):
+        self._find_ports()
+        print(f'available ports: {" ".join(self._available_ports)}')
+
+        print('find programmer')
+        self._find_programmer()
+        print(f'programmer: {self._programmer}')
+
+        print('find analyzer')
+        try:
+            self._find_analyzer()
+        except Exception as ex:
+            print(ex)
+        print(f'analyzer: {self._analyzer}')
+
+        return self._programmer and self._analyzer
+
+    def set_lpf_code(self, code: int):
+        return self._programmer.set_lpf_code(code)
+
+    def measure(self, code):
+        return self._analyzer.measure(code)
+
+    @property
+    def analyzer_addr(self):
+        return self._analyzer_addr
+
+    @analyzer_addr.setter
+    def analyzer_addr(self, addr):
+        self._analyzer_addr = addr
+
+
+class Task(QRunnable):
+
+    def __init__(self, end, fn, *args, **kwargs):
+        super().__init__()
+        self.end = end
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    @pyqtSlot()
+    def run(self):
+        self.fn(*self.args, **self.kwargs)
+        self.end()
+
+
+class Domain(QObject):
+
+    MAXREG = 127
+
+    codeMeasured = pyqtSignal()
+    measurementFinished = pyqtSignal()
+    statsReady = pyqtSignal()
+    harmonicMeasured = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self._instruments = InstrumentManager()
+        self.pool = QThreadPool()
+
+        self._code = 0
+        self._harmonic = 1
+
+        self._lastMeasurement = tuple()
+        self._lastFreqs = list()
+        self._lastAmps = list()
+
+        self.freqs = list()
+        self.amps = list()
+        self.codes = list()
+        self.cutoff_freqs = list()
+        self.loss_double_freq = list()
+        self.loss_triple_freq = list()
+        self.cutoff_freq_delta_x = list()
+        self.cutoff_freq_delta_y = list()
+
+        self._cutoffMag = -6
+
+        self.measurementFinished.connect(self._processStats)
+
+    def _clear(self):
+        self._lastFreqs.clear()
+        self._lastAmps.clear()
+        self.freqs.clear()
+        self.amps.clear()
+        self.codes.clear()
+        self.cutoff_freqs.clear()
+        self.loss_double_freq.clear()
+        self.loss_triple_freq.clear()
+        self.cutoff_freq_delta_x.clear()
+        self.cutoff_freq_delta_y.clear()
+
+    def findInstruments(self):
+        print('find instruments')
+        return self._instruments.find()
+
+    def measure(self):
+        print(f'run measurement, cutoff={self._cutoffMag}')
+        self._clear()
+        self.pool.start(Task(self.measurementFinished.emit, self._measureTask))
+
+    def _measureCode(self, harmonic=1, code=0):
+        print(f'\nmeasure: code={code:03d}, bin={code:07b}')
+        if not self._instruments.set_lpf_code(code):
+            print(f'error setting code: {code}')
+            return
+        self._lastMeasurement = self._instruments.measure(code)
+
+    def _measureTask(self):
+        print('start measurement task')
+        regs = self.MAXREG + 1
+
+        # MOCK
+        if def_mock:
+            regs = 5
+
+        with MeasureContext(self._instruments):
+            for code in range(regs):
+                time.sleep(0.5)
+                self._measureCode(code=code)
+                self._processCode()
+                self.codeMeasured.emit()
+
+        print('end measurement task')
+
+    def _parseFreqStr(self, string):
+        return [float(num) for num in string.split(',')]
+
+    def _parseAmpStr(self, string):
+        return [float(num) for idx, num in enumerate(string.split(',')) if idx % 2 == 0]
+
+    def _processCode(self):
+        print('processing code measurement')
+        self._lastFreqs = self._parseFreqStr(self._lastMeasurement[0])
+        self._lastAmps = self._parseAmpStr(self._lastMeasurement[1])
+
+        self.freqs.append(self._lastFreqs)
+        self.amps.append(self._lastAmps)
+
+    def _processStats(self):
+        print('process stats')
+        max_amp = max(map(max, self.amps))
+
+        cutoff_mag = max_amp + self._cutoffMag
+
+        for a, f in zip(self.amps, self.freqs):
+            cutoff_freq = f[a.index(min(a, key=lambda x: abs(cutoff_mag - x)))]
+            self.cutoff_freqs.append(cutoff_freq)
+
+            double_f = cutoff_freq * 2
+            triple_f = cutoff_freq * 3
+            double_f_index = f.index(min(f, key=lambda x: abs(double_f - x)))
+            triple_f_index = f.index(min(f, key=lambda x: abs(triple_f - x)))
+
+            self.loss_double_freq.append(a[double_f_index])
+            self.loss_triple_freq.append(a[triple_f_index])
+
+        self.cutoff_freqs = list(reversed(self.cutoff_freqs))
+        # TODO also reverse
+        # self.loss_double_freq.append(a[double_f_index])
+        # self.loss_triple_freq.append(a[triple_f_index])
+        self.codes = list(range(len(self.cutoff_freqs)))
+
+        for i in range(len(self.cutoff_freqs[:-1])):
+            d = abs(self.cutoff_freqs[i + 1] - self.cutoff_freqs[i])
+            self.cutoff_freq_delta_y.append(d)
+
+        self.cutoff_freq_delta_x = list(range(len(self.cutoff_freq_delta_y)))
+
+        self.statsReady.emit()
+
+    def measureHarmonic(self):
+        print(f'measure harmonic={self._harmonic}, code={self._code}')
+        with MeasureContext(self._instruments):
+            self._measureCode(harmonic=self._harmonic, code=self._code)
+            self._processCode()
+
+        self.harmonicMeasured.emit()
+
+    @property
+    def analyzerAddress(self):
+        return self._instruments.analyzer_addr
+
+    @analyzerAddress.setter
+    def analyzerAddress(self, addr):
+        print(f'set analyzer address {addr}')
+        self._instruments.analyzer_addr = addr
+
+    @property
+    def programmerName(self):
+        return str(self._instruments._programmer)
+
+    @property
+    def analyzerName(self):
+        return str(self._instruments._analyzer)
+
+    @property
+    def cutoffMag(self):
+        return self._cutoffMag
+
+    @cutoffMag.setter
+    def cutoffMag(self, value):
+        self._cutoffMag = value
+
+    @property
+    def canMeasure(self):
+        return self._instruments._analyzer and self._instruments._programmer
+
+    @property
+    def lastXs(self):
+        return self._lastFreqs
+
+    @property
+    def lastYs(self):
+        return self._lastAmps
+
+    @property
+    def cutoffXs(self):
+        return self.codes
+
+    @property
+    def cutoffYs(self):
+        return self.cutoff_freqs
+
+    @property
+    def deltaXs(self):
+        return self.cutoff_freq_delta_x
+
+    @property
+    def deltaYs(self):
+        return self.cutoff_freq_delta_y
+
+    @property
+    def lossDoubleXs(self):
+        return self.codes
+
+    @property
+    def lossDoubleYs(self):
+        return self.loss_double_freq
+
+    @property
+    def lossTripleXs(self):
+        return self.codes
+
+    @property
+    def lossTripleYs(self):
+        return self.loss_triple_freq
+
+    @property
+    def harmonicXs(self):
+        return self._lastFreqs
+
+    @property
+    def harmonicYs(self):
+        return self._lastAmps
+
+    @property
+    def harmonic(self):
+        return self._harmonic
+
+    @harmonic.setter
+    def harmonic(self, value):
+        self._harmonic = value
+
+    @property
+    def code(self):
+        return self._code
+
+    @code.setter
+    def code(self, value):
+        self._code = value
+
+
+
---
+++ mainwindow.py
@@ -0,0 +1,187 @@
+import errno
+import os
+import subprocess
+import xlsxwriter
+
+from PyQt5 import uic
+from PyQt5.QtGui import QRegularExpressionValidator
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtCore import Qt, pyqtSlot, QRegularExpression
+
+from domain import Domain
+from harmonicplotwidget import HarmonicPlotWidget
+from statplotwidget import StatPlotWidget
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setAttribute(Qt.WA_QuitOnClose)
+        self.setAttribute(Qt.WA_DeleteOnClose)
+
+        # create instance variables
+        self._ui = uic.loadUi("mainwindow.ui", self)
+
+        self._domain = Domain(parent=self)
+        self._statPlot = StatPlotWidget(parent=self, domain=self._domain)
+
+        self._harmonicPlot = HarmonicPlotWidget(parent=self, domain=self._domain)
+        self._ui.layHarmonic.addLayout(self._ui.codeWidget)
+        self._ui.layHarmonic.addWidget(self._harmonicPlot)
+
+        # TODO add harmonic plot widget
+
+        self._ui.tabCharts.insertTab(0, self._statPlot, 'Измерения')
+        # self._ui.tabCharts.setCurrentIndex(0)
+
+        self._init()
+
+    def _init(self):
+        validator = QRegularExpressionValidator(QRegularExpression(
+            '^TCPIP::(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}'
+            '([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])::INSTR$'))
+        self._ui.editAnalyzerAddr.setValidator(validator)
+
+        self._setupSignals()
+        self._setupControls()
+        self._refreshView()
+
+    def _setupSignals(self):
+        self._domain.statsReady.connect(self.on_statsReady)
+        self._domain.codeMeasured.connect(self.on_codeMeasured)
+        self._domain.harmonicMeasured.connect(self.on_harmonicMeasured)
+
+    def _setupControls(self):
+        pass
+
+    def _refreshView(self):
+        pass
+
+    def _modeFindInstr(self):
+        self._ui.btnMeasure.setEnabled(False)
+        self._ui.btnMeasureHarmonic.setEnabled(False)
+        self._ui.spinCutoffMagnitude.setEnabled(True)
+
+    def _modeMeasureReady(self):
+        self._ui.btnMeasure.setEnabled(True)
+        self._ui.btnMeasureHarmonic.setEnabled(True)
+        self._ui.spinCutoffMagnitude.setEnabled(True)
+
+    def _modeMeasureRunning(self):
+        self._ui.btnMeasure.setEnabled(False)
+        self._ui.btnMeasureHarmonic.setEnabled(False)
+        self._ui.spinCutoffMagnitude.setEnabled(False)
+
+    def _modeMeasureFinished(self):
+        self._ui.btnMeasure.setEnabled(True)
+        self._ui.btnMeasureHarmonic.setEnabled(True)
+        self._ui.spinCutoffMagnitude.setEnabled(True)
+
+    # event handlers
+    def resizeEvent(self, event):
+        self._refreshView()
+
+    def on_statsReady(self):
+        self._modeMeasureFinished()
+        self._statPlot.plotStats()
+
+    def on_codeMeasured(self):
+        self._statPlot.plotCode()
+
+    def on_harmonicMeasured(self):
+        self._harmonicPlot.plotHarmonic()
+
+    @pyqtSlot(str)
+    def on_editAnalyzerAddr_textChanged(self, text):
+        self._domain.analyzerAddress = text
+
+    @pyqtSlot(float)
+    def on_spinCutoffMagnitude_valueChanged(self, value):
+        self._domain.cutoffMag = value
+
+    @pyqtSlot()
+    def on_btnFindInstr_clicked(self):
+        if not self._domain.findInstruments():
+            QMessageBox.information(self, 'Ошибка', 'Инструменты не найдены, проверьте подключение.')
+            return
+
+        self._ui.editArduino.setText(self._domain.programmerName)
+        self._ui.editAnalyzer.setText(self._domain.analyzerName)
+        self._modeMeasureReady()
+
+    @pyqtSlot()
+    def on_btnMeasure_clicked(self):
+        if self._domain.canMeasure:
+            self._statPlot.clear()
+            self._modeMeasureRunning()
+            self._domain.measure()
+
+    @pyqtSlot()
+    def on_btnMeasureHarmonic_clicked(self):
+        self._domain.measureHarmonic()
+
+    @pyqtSlot(int)
+    def on_spinCode_valueChanged(self, value):
+        self._domain.code = value
+
+    @pyqtSlot(int)
+    def on_spinHarmonic_valueChanged(self, value):
+        self._domain.harmonic = value
+
+    @pyqtSlot()
+    def on_btnExportPng_clicked(self):
+        print('saving images')
+        path = ".\\image\\"
+        self._statPlot.save(path)
+        subprocess.call(f'explorer {path}', shell=True)
+        print('done')
+
+    @pyqtSlot()
+    def on_btnExportExcel_clicked(self):
+        print('export to excel')
+        to_export = [('cutoff_freq.xlsx', 'Код', 'Частота среза', self._domain.cutoffXs, self._domain.cutoffYs),
+                     ('cutoff_delta.xlsx', 'Код', 'Дельта', self._domain.deltaXs, self._domain.deltaYs)]
+
+        for ex in to_export:
+            self.export_to_excel(ex)
+
+        subprocess.call('explorer ' + '.\\excel\\', shell=True)
+
+    def export_to_excel(self, data):
+        fname, xname, yname, xdata, ydata = data
+
+        excel_path = ".\\excel\\"
+        try:
+            os.makedirs(excel_path)
+        except OSError as ex:
+            if ex.errno != errno.EEXIST:
+                raise
+
+        wb = xlsxwriter.Workbook(excel_path + fname)
+        ws = wb.add_worksheet("Sheet1")
+
+        ws.write("A1", xname)
+        ws.write("B1", yname)
+
+        start_row = 0
+        row = 0
+        for x, y in zip(xdata, ydata):
+            row += 1
+            ws.write(start_row + row, 0, x)
+            ws.write(start_row + row, 1, y)
+
+        chart = wb.add_chart({"type": "scatter",
+                              "subtype": "smooth"})
+        chart.add_series({"name": "Sheet1!$B$1",
+                          "categories": "=Sheet1!$A$2:$A$" + str(row + 1),
+                          "values": "=Sheet1!$B$2:$B$" + str(row + 1)})
+        chart.set_x_axis({"name": xname})
+        chart.set_y_axis({"name": yname})
+        ws.insert_chart("D3", chart)
+
+        wb.close()
+
+
+
